f4778d8e5c73b57396149efdd731d2a037332156
danielholanda <holand.daniel@gmail.com>
2016-05-12 18:26:48 +0000

    FIX - Hand is moving smooooth!

--- IMU/readImuDMP/readImuDMP.cpp
+++ IMU/readImuDMP/readImuDMP.cpp
@@ -6,6 +6,7 @@
 #include <math.h>
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
+#include <fcntl.h>  
 
 // class default I2C address is 0x68
 // specific I2C addresses may be passed as a parameter here
@@ -203,9 +204,24 @@ void loop() {
 	    imu2IsOK=false;
 	    char chProgramVar[20] = { (char)teapotPacket[0],(char)teapotPacket[1],(char)teapotPacket[2],(char)teapotPacket[3],(char)teapotPacket[4],(char)teapotPacket[5],(char)teapotPacket[6],(char)teapotPacket[7],(char)teapotPacket[8],(char)teapotPacket[9],(char)teapotPacket[10],(char)teapotPacket[11],(char)teapotPacket[12],(char)teapotPacket[13],(char)teapotPacket[14],(char)teapotPacket[15],(char)teapotPacket[16],(char)teapotPacket[17],(char)teapotPacket[18],(char)teapotPacket[19] };
 	    //char chProgramVar[] = "hello world" ;
-	    setenv("ShellVar", chProgramVar, 1) ;
-	    system("echo $ShellVar > /dev/ttyGS0");
+	    //setenv("ShellVar", chProgramVar, 1) ;
+	    //system("echo $ShellVar > /dev/ttyGS0");
 	    //printf("DataIsOk\n");
+
+	    char byte;
+	    int commHandle = open("/dev/ttyGS0", O_RDWR);
+	    write(commHandle, chProgramVar, 20);
+           
+            ssize_t size = read(commHandle, &byte, 1);
+	    printf("Read byte %c of size %d\n", byte,size);
+	    
+	    close(commHandle);
+
+	    //Reading thingers to vibrate
+	    //system("cat /dev/ttyGS0");
+	    //system("read $Shellvar < /dev/ttyGS0");
+	    //const char* s = getenv("Shellvar");
+	    //printf("Received %s \n", s);
     }
     
 }
