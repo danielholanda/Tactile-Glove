c166dd76c927f79f607b38f6cc76c5dedfcb5c19
Daniel Holanda Noronha <holand.daniel@gmail.com>
2016-11-07 18:22:09 -0100

    ADD - tactileGLove.cpp is nicely documented!

--- Galileo/tactileGlove.cpp
+++ Galileo/tactileGlove.cpp
@@ -1,3 +1,4 @@
+//All includes
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,11 +10,11 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
 #include <errno.h>
 #include <signal.h>
 #include "mraa.h"
 
+//Defining IO outputs of each finger
 #define thumb_f 8
 #define index_f 9
 #define middle_f 10
@@ -24,31 +25,24 @@
 // specific I2C addresses may be passed as a parameter here
 // AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
 // AD0 high = 0x69
-MPU6050 mpu_head(0x68);
-MPU6050 mpu_body(0x69);
+MPU6050 mpu_Arm(0x68);
+MPU6050 mpu_Hand(0x69);
 
-// uncomment "OUTPUT_READABLE_QUATERNION" if you want to see the actual
-// quaternion components in a [w, x, y, z] format (not best for parsing
-// on a remote host such as Processing or something though)
-//#define OUTPUT_READABLE_QUATERNION
 
-// uncomment "OUTPUT_TEAPOT" if you want output that matches the
-// format used for the InvenSense teapot demo
-#define OUTPUT_TEAPOT
 
 // MPU control/status vars
-bool dmpHeadReady = false;  // set true if DMP init was successful
-bool dmpBodyReady = false;  // set true if DMP init was successful
-uint8_t mpu_headIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t mpu_bodyIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t mpu_headStatus;      // return status after each device operation (0 = success, !0 = error)
-uint8_t mpu_bodyStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t headPacketSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t bodyPacketSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t headFifoCount;     // count of all bytes currently in FIFO
-uint8_t headFifoBuffer[64]; // FIFO storage buffer
-uint16_t bodyFifoCount;     // count of all bytes currently in FIFO
-uint8_t bodyFifoBuffer[64]; // FIFO storage buffer
+bool dmpArmReady = false;  		// set true if DMP init was successful
+bool dmpHandReady = false;  	
+uint8_t mpu_ArmIntStatus;   	// holds actual interrupt status byte from MPU
+uint8_t mpu_HandIntStatus;   	
+uint8_t mpu_ArmStatus;      	// return status after each device operation (0 = success, !0 = error)
+uint8_t mpu_HandStatus;      	
+uint16_t ArmPacketSize;    		// expected DMP packet size (default is 42 bytes)
+uint16_t HandPacketSize;    	
+uint16_t ArmFifoCount;     		// count of all bytes currently in FIFO
+uint16_t HandFifoCount;     	
+uint8_t ArmFifoBuffer[64]; 		// FIFO storage buffer
+uint8_t HandFifoBuffer[64]; 	
 
 // orientation/motion vars
 Quaternion q;           // [w, x, y, z]         quaternion container
@@ -59,14 +53,14 @@ VectorFloat gravity;    // [x, y, z]            gravity vector
 float euler[3];         // [psi, theta, phi]    Euler angle container
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
-// packet structure for InvenSense teapot demo
-uint8_t teapotPacket[20] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '\r', '\n' };
+// Packet structure 
+uint8_t quaternionPacket[20] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '\r', '\n' };
 
-//Checks is data is ready to send
+//Flags to indicate that data is ready
 bool imu1IsOK;
 bool imu2IsOK;
 
-//Prepares SOcket connection
+//Prepares Socket connection
 int clientSocket;
 struct sockaddr_in serverAddr;
 socklen_t addr_size;
@@ -74,228 +68,190 @@ socklen_t addr_size;
 //For GPIO 
 mraa_gpio_context gpio[5];
 
-// ================================================================
-// ===                      INITIAL SETUP                       ===
-// ================================================================
 
 void setup() {
 
 
+	//Socket configuration
+	clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(7891);
+	serverAddr.sin_addr.s_addr = inet_addr("10.0.0.106");
+	memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
-    clientSocket = socket(PF_INET, SOCK_STREAM, 0);
-
-    /*---- Configure settings of the server address struct ----*/
-    /* Address family = Internet */
-    serverAddr.sin_family = AF_INET;
-    /* Set port number, using htons function to use proper byte order */
-    serverAddr.sin_port = htons(7891);
-    /* Set IP address to localhost */
-    serverAddr.sin_addr.s_addr = inet_addr("10.0.0.106");
-    /* Set all bits of the padding field to 0 */
-    memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
-
-    /*---- Connect the socket to the server using the address struct ----*/
-    addr_size = sizeof serverAddr;
-    printf("Trying to connect to virtual environment...");
-    connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+    //Connect the socket to the server using the address struct
+	addr_size = sizeof serverAddr;
+	printf("Trying to connect to virtual environment...");
+	connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
 
 
     // Variables for checking if data is ready
-    imu1IsOK=false;
-    imu2IsOK=false;
-  
+	imu1IsOK=false;
+	imu2IsOK=false;
+
     // initialize device
-    printf("Initializing I2C devices...\n");
-    mpu_head.initialize();
-   //usleep(100);
-    mpu_body.initialize();
+	printf("Initializing I2C devices...\n");
+	mpu_Arm.initialize();
+	mpu_Hand.initialize();
 
     // verify connection
-    printf("Testing device connections...\n");
-    printf(mpu_head.testConnection() ? "MPU6050 connection successful\n" : "MPU6050 connection failed\n");
-    printf(mpu_body.testConnection() ? "MPU6050 connection successful\n" : "MPU6050 connection failed\n");
-    
-    // load and configure the DMP
-    printf("Initializing DMP...\n");
-    mpu_headStatus = mpu_head.dmpInitialize();
-    mpu_bodyStatus = mpu_body.dmpInitialize();
-    
-    // make sure it worked (returns 0 if so)
-    if (mpu_headStatus == 0 && mpu_bodyStatus == 0) {
-        // turn on the DMP, now that it's ready
-        printf("Enabling DMP...\n");
-        mpu_head.setDMPEnabled(true);
-        mpu_body.setDMPEnabled(true);
+	printf("Testing device connections...\n");
+	printf(mpu_Arm.testConnection() ? "MPU6050 arm connection successful\n" : "MPU6050 arm connection failed\n");
+	printf(mpu_Hand.testConnection() ? "MPU6050 hand connection successful\n" : "MPU6050 hand connection failed\n");
 
-        // enable Arduino interrupt detection
-        //Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
-        //attachInterrupt(0, dmpDataReady, RISING);
-        mpu_headIntStatus = mpu_head.getIntStatus();
-        mpu_bodyIntStatus = mpu_head.getIntStatus();
+    // load and configure the DMP
+	printf("Initializing DMP...\n");
+	mpu_ArmStatus = mpu_Arm.dmpInitialize();
+	mpu_HandStatus = mpu_Hand.dmpInitialize();
+
+    // make sure it worked (returns 0 if so)
+	if (mpu_ArmStatus == 0 && mpu_HandStatus == 0) {
+        // turn on the DMP, now that it's ready
+		printf("Enabling DMP...\n");
+		mpu_Arm.setDMPEnabled(true);
+		mpu_Hand.setDMPEnabled(true);
+
+        // Get Interruption Status
+		mpu_ArmIntStatus = mpu_Arm.getIntStatus();
+		mpu_HandIntStatus = mpu_Arm.getIntStatus();
 
         // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        printf("DMP ready!\n");
-        dmpHeadReady = true;
-        dmpBodyReady = true;
+		printf("DMP ready!\n");
+		dmpArmReady = true;
+		dmpHandReady = true;
 
         // get expected DMP packet size for later comparison
-        headPacketSize = mpu_head.dmpGetFIFOPacketSize();
-        bodyPacketSize = mpu_body.dmpGetFIFOPacketSize();
-    } else {
+		ArmPacketSize = mpu_Arm.dmpGetFIFOPacketSize();
+		HandPacketSize = mpu_Hand.dmpGetFIFOPacketSize();
+	} else {
         // ERROR!
         // 1 = initial memory load failed
         // 2 = DMP configuration updates failed
         // (if it's going to break, usually the code will be 1)
-        printf("DMP Initialization failed (code %d)\n", mpu_headStatus);
-        printf("DMP Initialization failed (code %d)\n", mpu_bodyStatus);
-        
-    }
+		printf("DMP Arm Initialization failed (code %d)\n", mpu_ArmStatus);
+		printf("DMP Hand Initialization failed (code %d)\n", mpu_HandStatus);
 
-    //Setting GPIO	
-    mraa_init();
-    fprintf(stdout, "MRAA Version: %s\n", mraa_get_version());
-    gpio[0] = mraa_gpio_init(thumb_f);
-    gpio[4] = mraa_gpio_init(index_f);
-    gpio[3] = mraa_gpio_init(middle_f);
-    gpio[2] = mraa_gpio_init(ring_f);
-    gpio[1] = mraa_gpio_init(pinky_f);
+	}
 
-    // set direction to OUT
-    mraa_gpio_dir(gpio[0], MRAA_GPIO_OUT);
-    mraa_gpio_dir(gpio[1], MRAA_GPIO_OUT);
-    mraa_gpio_dir(gpio[2], MRAA_GPIO_OUT);
-    mraa_gpio_dir(gpio[3], MRAA_GPIO_OUT);
-    mraa_gpio_dir(gpio[4], MRAA_GPIO_OUT);
-    
+    // Setting GPIO (Order is changed due to the way fingers are drawed on Processing)
+	mraa_init();
+	fprintf(stdout, "MRAA Version: %s\n", mraa_get_version());
+	gpio[0] = mraa_gpio_init(thumb_f);
+	gpio[4] = mraa_gpio_init(index_f);
+	gpio[3] = mraa_gpio_init(middle_f);
+	gpio[2] = mraa_gpio_init(ring_f);
+	gpio[1] = mraa_gpio_init(pinky_f);
+
+    // Set GPIO direction to OUT
+	mraa_gpio_dir(gpio[0], MRAA_GPIO_OUT);
+	mraa_gpio_dir(gpio[1], MRAA_GPIO_OUT);
+	mraa_gpio_dir(gpio[2], MRAA_GPIO_OUT);
+	mraa_gpio_dir(gpio[3], MRAA_GPIO_OUT);
+	mraa_gpio_dir(gpio[4], MRAA_GPIO_OUT);
+
 }
 
 
-// ================================================================
-// ===                    MAIN PROGRAM LOOP                     ===
-// ================================================================
 
 void loop() {
-    // if programming failed, don't try to do anything
-    if (dmpHeadReady)
-    {
-        // get current FIFO count
-        headFifoCount = mpu_head.getFIFOCount();
-
-        if (headFifoCount == 1024) {
-            // reset so we can continue cleanly
-            mpu_head.resetFIFO();
-            printf("FIFO overflow!\n");
-
-        // otherwise, check for DMP data ready interrupt (this should happen frequently)
-        } else if (headFifoCount >= 42) {
-            // read a packet from FIFO
-            mpu_head.getFIFOBytes(headFifoBuffer, headPacketSize);
-
-            #ifdef OUTPUT_READABLE_QUATERNION
-                // display quaternion values in easy matrix form: w x y z
-                mpu_head.dmpGetQuaternion(&q, headFifoBuffer);
-                printf("IMU1: quat %7.2f %7.2f %7.2f %7.2f    ", q.w,q.x,q.y,q.z);
-            #endif
-
-            #ifdef OUTPUT_TEAPOT
-                // display quaternion values in InvenSense Teapot demo format:
-                teapotPacket[2] = headFifoBuffer[0];
-                teapotPacket[3] = headFifoBuffer[1];
-                teapotPacket[4] = headFifoBuffer[4];
-                teapotPacket[5] = headFifoBuffer[5];
-                teapotPacket[6] = headFifoBuffer[8];
-                teapotPacket[7] = headFifoBuffer[9];
-                teapotPacket[8] = headFifoBuffer[12];
-                teapotPacket[9] = headFifoBuffer[13];
-                //teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
-		imu1IsOK=true;
-            #endif
-            //printf("\n");
-        }
-    }
     
-    
-    if (dmpBodyReady)
-    {
-        // get current FIFO count
-        bodyFifoCount = mpu_body.getFIFOCount();
+	//Reads IMU data from Arm
+	if (dmpArmReady)
+	{
+        // Get current FIFO count
+		ArmFifoCount = mpu_Arm.getFIFOCount();
 
-        if (bodyFifoCount == 1024) {
-            // reset so we can continue cleanly
-            mpu_body.resetFIFO();
-            printf("FIFO overflow!\n");
+		if (ArmFifoCount == 1024) {
+            // Reset so we can continue cleanly
+			mpu_Arm.resetFIFO();
+			printf("FIFO overflow!\n");
 
-        // otherwise, check for DMP data ready interrupt (this should happen frequently)
-        } else if (bodyFifoCount >= 42) {
-            // read a packet from FIFO
-            mpu_body.getFIFOBytes(bodyFifoBuffer, bodyPacketSize);
+        // Otherwise, check for DMP data ready interrupt (this should happen frequently)
+		} else if (ArmFifoCount >= 42) {
+            // Read a packet from FIFO
+			mpu_Arm.getFIFOBytes(ArmFifoBuffer, ArmPacketSize);
 
-            #ifdef OUTPUT_READABLE_QUATERNION
-                // display quaternion values in easy matrix form: w x y z
-                mpu_body.dmpGetQuaternion(&q, bodyFifoBuffer);
-                printf("IMU2: quat %7.2f %7.2f %7.2f %7.2f    ", q.w,q.x,q.y,q.z);
-            #endif
+            // Create Packet
+			quaternionPacket[2] = ArmFifoBuffer[0];
+			quaternionPacket[3] = ArmFifoBuffer[1];
+			quaternionPacket[4] = ArmFifoBuffer[4];
+			quaternionPacket[5] = ArmFifoBuffer[5];
+			quaternionPacket[6] = ArmFifoBuffer[8];
+			quaternionPacket[7] = ArmFifoBuffer[9];
+			quaternionPacket[8] = ArmFifoBuffer[12];
+			quaternionPacket[9] = ArmFifoBuffer[13];
+			imu1IsOK=true;
 
-            #ifdef OUTPUT_TEAPOT
-                // display quaternion values in InvenSense Teapot demo format:
-                teapotPacket[10] = bodyFifoBuffer[0];
-                teapotPacket[11] = bodyFifoBuffer[1];
-                teapotPacket[12] = bodyFifoBuffer[4];
-                teapotPacket[13] = bodyFifoBuffer[5];
-                teapotPacket[14] = bodyFifoBuffer[8];
-                teapotPacket[15] = bodyFifoBuffer[9];
-                teapotPacket[16] = bodyFifoBuffer[12];
-                teapotPacket[17] = bodyFifoBuffer[13];
-                //teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
-		imu2IsOK=true;
-            #endif
-            printf("\n");
-        }
-    }
-
-    if(imu1IsOK && imu2IsOK){
-	    //Serial.write(teapotPacket, 20);
-	    imu1IsOK=false;
-	    imu2IsOK=false;
-    
-    	printf("DataIsOk\n");
-
-
-   	send(clientSocket,teapotPacket,sizeof(teapotPacket),0);
-	
-    
-
-    	char buffer_recv[1];
-    	recv(clientSocket, buffer_recv, sizeof(buffer_recv), 0);
-    	printf("Data received: %s\n",buffer_recv);
-    	
-	uint8_t finger = (uint8_t)buffer_recv[0];
-	
-
-	for(int i=0;i<5;i++){
-		if(finger&(1<<i)){
-			mraa_gpio_write(gpio[i],1);
-			printf("1_");
-
-		}else{
-			mraa_gpio_write(gpio[i],0);
-			printf("0_");
 		}
 	}
 
+	//Reads IMU data from Hand
+	if (dmpHandReady)
+	{
+        // Get current FIFO count
+		HandFifoCount = mpu_Hand.getFIFOCount();
 
+		if (HandFifoCount == 1024) {
+            // Reset so we can continue cleanly
+			mpu_Hand.resetFIFO();
+			printf("FIFO overflow!\n");
 
-    	
-    }
+        // Otherwise, check for DMP data ready interrupt (this should happen frequently)
+		} else if (HandFifoCount >= 42) {
+            // Read a packet from FIFO
+			mpu_Hand.getFIFOBytes(HandFifoBuffer, HandPacketSize);
+
+            // Create Packet
+			quaternionPacket[10] = HandFifoBuffer[0];
+			quaternionPacket[11] = HandFifoBuffer[1];
+			quaternionPacket[12] = HandFifoBuffer[4];
+			quaternionPacket[13] = HandFifoBuffer[5];
+			quaternionPacket[14] = HandFifoBuffer[8];
+			quaternionPacket[15] = HandFifoBuffer[9];
+			quaternionPacket[16] = HandFifoBuffer[12];
+			quaternionPacket[17] = HandFifoBuffer[13];
+			imu2IsOK=true;
+
+			printf("\n");
+		}
+	}
+
+	//Data is OK if I can read from both IMUs
+	if(imu1IsOK && imu2IsOK){
+
+		//Set flags to false
+		imu1IsOK=false;
+		imu2IsOK=false;
+
+		printf("DataIsOk\n");
+
+		//Send message through socket
+		send(clientSocket,quaternionPacket,sizeof(quaternionPacket),0);
+
+		//Read incoming message saying fingers to vibrate
+		char buffer_recv[1];
+		recv(clientSocket, buffer_recv, sizeof(buffer_recv), 0);
+		uint8_t finger = (uint8_t)buffer_recv[0];
+
+		//Vibrate fingers according to message received
+		for(int i=0;i<5;i++){
+			if(finger&(1<<i)){
+				mraa_gpio_write(gpio[i],1);
+
+			}else{
+				mraa_gpio_write(gpio[i],0);
+			}
+		}
+	}
 }
 
 int main() {
-    setup();
-    usleep(100000);
-    
-    for (;;)
-        loop();
-    
-    return 0;
+	setup();
+	usleep(100000);
+
+	for (;;)
+		loop();
+
+	return 0;
 }
 
